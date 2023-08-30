1eedc5a0e5862009f2f42b59a30f580276b8cd96
triistam <isakulovdev@gmail.com>
2023-08-30 23:59:53 +0500

    a little text about me

--- pages/about.vue
+++ pages/about.vue
@@ -7,5 +7,16 @@ useHead({
 <template>
     <div class="bg-neutral-950 text-white height">
         <Header></Header>
+        <div class="container w-4/6 mx-auto">
+          <h1 class="text-center text-6xl Chomsky">About me.</h1>
+          <p class="text-3xl JetBrainsMono m-5">I am 17 years old enthusiast developer from Uzbekistan. I have started my career since 2020.
+            I have experienced several Telegram Bot frameworks, VueJS/NuxtJS, several backend frameworks like 
+            Django, Flask, ExpressJS. I use several programming languages as JavaScript, TypeScript, Python3,
+            Rust and Shell for scripting purposes. I'm planning to learn more about C/C++ and Zig.
+            Doing some design works with Figma. I'm active linux user. I have tried more than 8 linux distributions like
+            ArchLinux, EndeavourOS, ArcoLinux, Manjaro, Fedora, Ubuntu, Lubuntu, Linux Mint. I can communicate in English(b2) and Uzbek(native) as well, learning
+            Russian nowadays. 
+          </p>
+        </div>
     </div>
 </template>
