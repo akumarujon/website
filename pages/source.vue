0370be2267a5a9811af7837577458c51a7f5b569
triistam <isakulovdev@gmail.com>
2023-08-31 10:24:44 +0500

    redirect script to source code

---
+++ pages/source.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- Your page content here -->
+    </div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next) {
+        if (process.client && to.path === '/source') {
+            window.location.href = 'https://github.com/akumarujon/website';
+        } else {
+            next();
+        }
+    }
+};
+</script>
+  
