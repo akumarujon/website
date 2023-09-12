5e31042895b644f2ecac1c8218c5fd77b6f61c78
akumarujon <isakulovdev@gmail.com>
2023-09-12 19:46:29 +0500

    added map & more issues and fixed some issues

--- components/Header.vue
+++ components/Header.vue
@@ -1,41 +1,37 @@
 <script setup lang="ts">
 useHead({
-  meta: [
+    meta: [
         { name: 'og:description', content: "Konichiwa, I'm akumarujon, Enthusiast Developer from Uzbekistan. This is my personal website. You can find more information about me here." },
         { name: 'description', content: "Konichiwa, I'm akumarujon, Enthusiast Developer from Uzbekistan. This is my personal website. You can find more information about me here." },
-        { name: "og:site_name", content: "akumarujon UwU"},
-        { name: "og:url", content: "https://akumarujon.uz"},
-        { name: "og:image", content: "https://akumarujon.uz/og.png"},
-        { name: "viewport", content: "width=device-width, initial-scale=1.0"},
-        { name: "twitter:image", content: "https://akumarujon.uz/og.png"},
-        { name: "twitter:card", content: "summary_large_image"},
+        { name: "og:site_name", content: "akumarujon UwU" },
+        { name: "og:url", content: "https://akumarujon.uz" },
+        { name: "og:image", content: "https://akumarujon.uz/og.png" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { name: "twitter:image", content: "https://akumarujon.uz/og.png" },
+        { name: "twitter:card", content: "summary_large_image" },
     ],
 })
 </script>
 
 <template>
-    <div class="grey text-white  w-100 p-10 Chomsky">
+    <div class="grey text-white w-100 p-10 Chomsky">
         <div class="container flex mx-auto justify-between items-center">
-        <NuxtLink to="/">
-            <h1 class="text-5xl mx-5">akumarujon's</h1>
-        </NuxtLink>
+            <NuxtLink to="/">
+                <h1 class="text-5xl mx-5">akumarujon's</h1>
+            </NuxtLink>
             <ul class="mx-5 text-3xl">
-            <li>
-                <NuxtLink to="/" class="link">Home</NuxtLink>
-                <NuxtLink to="/projects" class="link">Projects</NuxtLink>
-                <NuxtLink to="/about" class="link">About</NuxtLink>
-                <NuxtLink to="/blog" class="link">Blog</NuxtLink>
-                <NuxtLink to="/source" class="link">Source</NuxtLink>
-            </li>
-        </ul>
-    </div>
+                <li>
+                    <NuxtLink to="/map" class="link">Map</NuxtLink>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .link {
-   margin: 20px;
-   font-size: 20px;
+    margin: 20px;
+    font-size: 20px;
 }
 
 * {
---
+++ pages/map.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+useHead({
+    title: 'akumarujon - Map',
+    htmlAttrs: {
+        lang: 'en',
+    },
+})
+</script>
+
+<template>
+    <div class="grey text-white height">
+        <Header></Header>
+        <div class="w-4/6 mx-auto Chomsky justify-center text-center">
+            <h1 class="text-5xl mb-5">Map for my website.</h1>
+            <NuxtLink to="/" class="link">Home</NuxtLink>
+            <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+            <NuxtLink to="/about" class="link">About</NuxtLink>
+            <NuxtLink to="/blog" class="link">Blog</NuxtLink>
+            <NuxtLink to="/source" class="link">Source</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.link {
+    display: block;
+    font-size: 50px !important;
+}
+</style>
