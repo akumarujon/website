a4685240c2187e28ef77b7b06c728d266f16fc88
akumarujon <isakulovdev@gmail.com>
2023-09-13 18:03:29 +0500

    add ~responsive h1

--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,9 @@ useHead({
 <template>
   <Header></Header>
     <div class="grey text-white height" style="margin: 0 auto; max-width: 1440px;">
-      <div class="w-4/6 ml-20 text flex flex-col justify-center items-start">
-        <div>
-          <h1 class="text-9xl Chomsky  mt-6">akumarujon</h1>
+      <div class=" ml-20 text flex flex-col justify-center items-start ">
+        <div class="text-center">
+          <h1 class="lg:text-9xl Chomsky text-center md:text-6xl sm:text-3xl mt-6">akumarujon</h1>
         </div>
         <p class="text-2xl JetBrainsMono">Enthusiast Developer from Uzbekistan.</p>
       </div>
