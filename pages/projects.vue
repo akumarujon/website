1b9903a52fa2ab183f6a5ea34063c029f8828b9b
akumaru <isakulovdev@gmail.com>
2023-09-22 17:46:23 +0500

    show projects in a page using -for

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
+import json from "../projects.json";
+
 useHead({
   title: 'akumarujon - Projects',
   htmlAttrs: {
     lang: 'en',
   },
 })
+
+const projects = ref(json);
 </script>
 
 <template>
     <div class="grey text-white height">
         <Header></Header>
-        <div class="w-4/6 mx-auto">
-          <h1 class="text-center text-3xl">Projects page is not finished because of my poor project base. I'll fill this page after finishing some of my projects. I'm sorry.</h1>
+        <div class="w-2/5 mx-auto">
+        <div class="project" v-for="project in projects">
+          <NuxtLink :to="`${project.url}`">
+            <h1 class="text-2xl">{{ project.name }}</h1>
+            <p>{{ project.description }}</p>
+            <p>Language: {{ project.language }}</p>
+            <p>Version: {{ project.version }}</p>
+          </NuxtLink>
+          </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.project{
+  border-radius: 10px;
+  margin: 20px;
+  padding: 20px;
+  border: 2px solid white;
+}
+</style>
