d32cbe04dcbc9b016cba60a622aff006d248221e
akumaru <isakulovdev@gmail.com>
2024-02-07 10:56:54 +0500

    remove the text

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@ useHead({
     <div class="grey text-white" style="margin: 0 auto; max-width: 1440px;">
       <div class="text flex flex-col justify-center items-center">
         <h1 class="Chomsky text-center mt-6 header" style="font-size: 70px !important;">akumarujon</h1>
-        <p class="JetBrainsMono text-center mx-20">Self-taught Developer from Uzbekistan. I'm active linux user. I have tried more than 8 linux distributions like ArchLinux, EndeavourOS, ArcoLinux, Manjaro, Fedora, Ubuntu, Lubuntu, Linux Mint. I can communicate in English(b2) and Uzbek(native) as well, learning Russian nowadays. I use several programming languages as JavaScript, TypeScript, Python3, Rust and Shell for scripting purposes. I have experienced several Telegram Bot frameworks, VueJS/NuxtJS, several backend frameworks like Django, Flask, ExpressJS. </p>
+        <p class="JetBrainsMono text-center mx-20">Self-taught Developer from Uzbekistan. </p>
       </div>
     </div>
   </div>
